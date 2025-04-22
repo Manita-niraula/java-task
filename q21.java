a449910c49de4f6fa9d832d06f25926a96c57018
manita <manitaniraula046@gmail.com>
2025-04-22 12:03:50 +0545

    Update q21.java

--- q21.java
+++ q21.java
@@ -1,4 +1,20 @@
 //21 Write a program to find the sum of digits of a number using a loop.
+import java.util.Scanner;
 public class q21 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int sum = 0;
+        int temp = Math.abs(number); 
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += digit;
+            temp /= 10;
+        }
+        System.out.println("Sum of digits of " + number + " is: " + sum);
+
+        scanner.close();
+    }
 }
